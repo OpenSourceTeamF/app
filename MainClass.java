3786f05d1555b1444fa0d8f7a032d3da6b97db92
Jinho Kim <angry123456@nate.com>
2019-11-14 15:38:04 +0900

    add Mainclass java

--- MainClass.java
+++ MainClass.java
@@ -9,12 +9,38 @@ import java.util.ArrayList;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import java.util.HashMap;
 
 
 public class MainClass{
 	private ServerSocket server;
 
 	ArrayList<UserClass> user_list;
+
+	HashMap[] problems = {
+		new HashMap(){{
+			put("question","1+2=?");
+			put("answer","3");
+		}},
+		new HashMap(){{
+			put("question","11+22=?");
+			put("answer","33");
+		}},
+		new HashMap(){{
+			put("question","4+6=?");
+			put("answer","10");
+		}}
+
+	};
+	int problemNumber = 0;
+	String question = "";
+	String answer = "";
+	int score = 0;
+
+	int totalCorrect = 0;
+
+
+
 	public static void main(String[] args)
 	{
 		new MainClass();
@@ -28,6 +54,8 @@ public class MainClass{
 
 			ConnectionThread thread= new ConnectionThread();
 			thread.start();
+
+
 		}catch(Exception e){e.printStackTrace();}
 	}
 
@@ -72,9 +100,18 @@ public class MainClass{
 				dos.writeUTF("server : Welcome "+nickName+"!");
 				sendToClient("server : "+nickName+" is connected");
 
-				UserClass user = new UserClass(nickName,socket);
+				UserClass user = new UserClass(nickName,socket,score);
 				user.start();
 				user_list.add(user);
+	
+				if(user_list.size()==2){
+					sendToClient("server : game start!!");
+					question = (String)problems[0].get("question");
+					answer = (String)problems[0].get("answer");
+					problemNumber++;
+					sendToClient("problem1 : "+question);
+				}
+				
 	
 			}catch(Exception e){e.printStackTrace();}
 
@@ -84,15 +121,17 @@ public class MainClass{
 
 
 	class UserClass extends Thread{
+		int score;
 		String nickName;
 		Socket socket;
 		DataInputStream dis;
 		DataOutputStream dos;
 
-		public UserClass(String nickName, Socket socket){
+		public UserClass(String nickName, Socket socket,int score){
 			try{
 				this.nickName = nickName;
 				this.socket = socket;
+				this.score = score;
 				InputStream is=socket.getInputStream();
 				OutputStream os = socket.getOutputStream();
 				dis = new DataInputStream(is);
@@ -105,9 +144,47 @@ public class MainClass{
 		public void run(){
 			try{
 				while(true){
+					
+
 					String msg = dis.readUTF();
 					sendToClient(nickName+" : "+msg);
-				
+
+					if(msg.equals(answer)){
+						score++;
+						sendToClient(nickName+" Correct! score : "+score);
+			
+						if(problemNumber == 3){
+							Quiz test = new Quiz(nickName,socket,score);
+							test.start();
+
+							String user0 = user_list.get(0).nickName;
+							int user0_s = user_list.get(0).score;
+							String user1 = user_list.get(1).nickName;
+							int user1_s = user_list.get(1).score;
+							sendToClient("[TEST2]"+user0+" score:"+user0_s);
+							sendToClient("[TEST2]"+user1+" score:"+user1_s);
+						
+							if(user0_s<user1_s){
+								sendToClient(user1+" win!!");
+							}		
+							else if(user0_s==user1_s){
+								sendToClient("Draw!!");
+							}
+							else{
+								sendToClient(user0+"win!!");
+							}
+							problemNumber = 0;
+						}
+
+						//give new question	to users
+						question = (String)problems[problemNumber].get("question");
+						answer = (String)problems[problemNumber].get("answer");
+						problemNumber++;
+						sendToClient("test answer | "+question);
+					}
+					else{
+						sendToClient(nickName+" try again!");
+					}
 				}
 	
 			}catch(Exception e){e.printStackTrace();}
@@ -116,6 +193,27 @@ public class MainClass{
 
 
 	}	
+
+
+	class Quiz extends Thread{
+		String nickName;
+		Socket socket;
+		int score;
+	
+		public Quiz(String nickName, Socket socket,int score){
+			try{
+				this.nickName = nickName;
+				this.socket = socket;
+				this.score = score;
+			}catch(Exception e){e.printStackTrace();}
+		}
+		
+		public void run(){
+			try{
+				sendToClient("[Test]"+nickName+" score : "+score);
+			}catch(Exception e){e.printStackTrace();}
+		}
+	}
 
 
 	public synchronized void sendToClient(String msg){
