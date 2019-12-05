b9774d7d08f4c8ad774e1623f5e4d9061a460ec1
Jinho Kim <angry123456@nate.com>
2019-12-05 15:12:36 +0900

    version

---
+++ version1/MainActivity.java
@@ -0,0 +1,211 @@
+package com.example.quiz_app;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isConnect = false;
+    EditText edit1;
+    Button btn1;
+    LinearLayout container;
+    ScrollView scroll;
+    ProgressDialog pro;
+    TextView toptext;
+    TextView firstplace;
+
+    boolean isRunning=false;
+
+    Socket member_socket;
+
+    String user_nickname;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edit1 = findViewById(R.id.editText);
+        btn1 = findViewById(R.id.button);
+        container = findViewById(R.id.container);
+        scroll = findViewById(R.id.scroll);
+        toptext = findViewById(R.id.TopText);
+        firstplace = findViewById(R.id.FirstPlace);
+    }
+
+
+    public void btnMethod(View v){
+        if(!isConnect){
+            String nickName = edit1.getText().toString();
+            if(nickName.length()>0 && nickName != null){
+                pro = ProgressDialog.show(this, null, "connecting...");
+
+                ConnectionThread thread = new ConnectionThread();
+                thread.start();
+            }
+
+            else{
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Input your NickName");
+                builder.setPositiveButton("Check",null);
+                builder.show();
+            }
+        }
+        else{
+            String msg = edit1.getText().toString();
+
+            SendToServerThread thread = new SendToServerThread(member_socket,msg);
+            thread.start();
+        }
+    }
+
+
+    class ConnectionThread extends Thread{
+        @Override
+        public void run(){
+            try{
+                final Socket socket = new Socket("192.168.162.128",30000);
+                member_socket=socket;
+
+                String nickName = edit1.getText().toString();
+                user_nickname=nickName;
+
+                OutputStream os = socket.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(os);
+
+                dos.writeUTF(nickName);
+
+                runOnUiThread(new Runnable(){
+                    @Override
+                    public void run(){
+                        pro.dismiss();
+                        edit1.setText("");
+                        edit1.setHint("Input Message");
+                        btn1.setText("전송");
+
+                        isConnect = true;
+                        isRunning = true;
+
+                        MessageThread thread = new MessageThread(socket);
+                        thread.start();
+                    }
+                });
+            }catch(Exception e){e.printStackTrace();}
+        }
+    }
+
+    class MessageThread extends Thread{
+        Socket socket;
+        DataInputStream dis;
+
+        public MessageThread(Socket socket){
+            try{
+                this.socket = socket;
+                InputStream is = socket.getInputStream();
+                dis = new DataInputStream(is);
+            }catch(Exception e){e.printStackTrace();}
+        }
+
+        @Override
+        public void run(){
+            try{
+                while(isRunning){
+                    final String msg = dis.readUTF();
+                    runOnUiThread(new Runnable(){
+                        @Override
+                        public void run(){
+                            TextView tv = new TextView(MainActivity.this);
+                            tv.setTextColor(Color.BLACK);
+                            tv.setTextSize(TypedValue.COMPLEX_UNIT_SP,22);
+
+                            if(msg.startsWith(user_nickname)){
+                                if(msg.contains("Question")||msg.contains("Hint")){
+                                    toptext.setText(msg);
+                                }
+                                if(msg.contains("first")){
+                                    firstplace.setText(msg);
+                                }
+                                tv.setBackgroundResource(R.drawable.me);
+                            }
+                            else{
+                                if(msg.contains("Question")||msg.contains("Hint")){
+                                    toptext.setText(msg);
+                                }
+                                if(msg.contains("First")){
+                                    firstplace.setText(msg);
+                                }
+                                tv.setBackgroundResource(R.drawable.you);
+                            }
+                            tv.setText(msg);
+
+                            container.addView(tv);
+
+                            scroll.fullScroll(View.FOCUS_DOWN);
+                        }
+                    });
+                }
+            }catch (Exception e){e.printStackTrace();}
+        }
+    }
+
+    class SendToServerThread extends Thread{
+        Socket socket;
+        String msg;
+        DataOutputStream dos;
+
+        public SendToServerThread(Socket socket, String msg){
+            try{
+                this.socket=socket;
+                this.msg=msg;
+                OutputStream os = socket.getOutputStream();
+                dos = new DataOutputStream(os);
+            }catch(Exception e){e.printStackTrace();}
+        }
+
+        @Override
+        public void run(){
+            try{
+                dos.writeUTF(msg);
+                runOnUiThread(new Runnable(){
+                    @Override
+                    public void run(){
+                        edit1.setText("");
+                    }
+                });
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        try{
+            member_socket.close();
+            isRunning=false;
+
+        }catch (Exception e){e.printStackTrace();}
+    }
+
+}
---
+++ version1/MainClass.java
@@ -0,0 +1,277 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import java.util.HashMap;
+
+
+public class MainClass{
+	private ServerSocket server;
+
+	ArrayList<UserClass> user_list;
+
+	HashMap[] problems = {
+		new HashMap(){{
+			put("question","1+2=?");
+			put("answer","3");
+			put("hint1","three");
+			put("hint2","third");
+		}},
+		new HashMap(){{
+			put("question","25+15=?");
+			put("answer","40");
+			put("hint1","forty");
+			put("hint2","40");
+		}},
+		new HashMap(){{
+			put("question","4+6=?");
+			put("answer","10");
+			put("hint1","ten");
+			put("hint2","x");
+		}}
+
+	};
+	int problemNumber = 0;
+	String question = "";
+	String answer = "";
+	String hint1 = "";
+	String hint2 = "";
+	int score = 0;
+	int incorrect = 0;
+	String  winner;
+	int w_score;
+
+	int totalCorrect = 0;
+
+
+
+	public static void main(String[] args)
+	{
+		new MainClass();
+	}		
+
+
+	public MainClass(){
+		try{
+			user_list=new ArrayList<UserClass>();
+			server=new ServerSocket(30000);
+
+			ConnectionThread thread= new ConnectionThread();
+			thread.start();
+
+
+		}catch(Exception e){e.printStackTrace();}
+	}
+
+
+
+	class ConnectionThread extends Thread{
+
+		@Override
+		public void run(){
+			try{
+				while(true){
+					System.out.println("waiting connection of users");
+					InetAddress local = InetAddress.getLocalHost();
+					System.out.println(local.getHostAddress());
+					Socket socket=server.accept();
+					System.out.println("user connected");
+					NickNameThread thread = new NickNameThread(socket);
+
+					thread.start();
+				}
+			}catch(Exception e){e.printStackTrace();}
+		}
+	}
+
+
+	class NickNameThread extends Thread{
+		private Socket socket;
+
+		public NickNameThread(Socket socket){
+			this.socket=socket;	
+		}		
+		
+		public void run(){
+			try{
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				DataInputStream dis = new DataInputStream(is);
+				DataOutputStream dos = new DataOutputStream(os);
+
+				String nickName=dis.readUTF();
+
+				dos.writeUTF("[server] Welcome "+nickName+"!");
+				sendToClient("[server] "+nickName+" is connected");
+
+				UserClass user = new UserClass(nickName,socket,score);
+				user.start();
+				user_list.add(user);
+	
+				if(user_list.size()==2){
+					sendToClient("[server] game start!!");
+					question = (String)problems[0].get("question");
+					answer = (String)problems[0].get("answer");
+					hint1 = (String)problems[0].get("hint1");
+					hint2 = (String)problems[0].get("hint2");
+					problemNumber++;
+					sendToClient("Question | "+question);
+				}
+				
+	
+			}catch(Exception e){e.printStackTrace();}
+
+		}
+
+	}
+
+
+	class UserClass extends Thread{
+		int score;
+		String nickName;
+		Socket socket;
+		DataInputStream dis;
+		DataOutputStream dos;
+
+		public UserClass(String nickName, Socket socket,int score){
+			try{
+				this.nickName = nickName;
+				this.socket = socket;
+				this.score = score;
+				InputStream is=socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				dis = new DataInputStream(is);
+				dos = new DataOutputStream(os);
+
+			}catch(Exception e){ e.printStackTrace();}
+		
+		}
+
+		public void run(){
+			try{
+				while(true){
+					
+					String msg = dis.readUTF();
+					sendToClient("["+nickName+"] "+msg);
+
+					if(msg.equals(answer)){
+						score++;
+						sendToClient("[server] "+nickName+" Correct! score : "+score);
+			
+						if(problemNumber == 3){
+							/*
+							Quiz test = new Quiz(nickName,socket,score);
+							test.start();
+
+							String user0 = user_list.get(0).nickName;
+							int user0_s = user_list.get(0).score;
+							String user1 = user_list.get(1).nickName;
+							int user1_s = user_list.get(1).score;
+							sendToClient("[TEST2]"+user0+" score:"+user0_s);
+							sendToClient("[TEST2]"+user1+" score:"+user1_s);
+						
+							if(user0_s<user1_s){
+								sendToClient(user1+" win!!");
+							}		
+							else if(user0_s==user1_s){
+								sendToClient("Draw!!");
+							}
+							else{
+								sendToClient(user0+"win!!");
+							}*/
+							problemNumber = 0;
+						}
+
+						//define first place
+						w_score = 0;
+						for(int i=0;i<user_list.size();i++){
+							if(user_list.get(i).score > w_score){
+								winner = user_list.get(i).nickName;
+								w_score = user_list.get(i).score;
+							}
+						}
+						sendToClient("First Place : "+winner+"(score : "+w_score+")");
+
+
+						//give new question	to users
+						question = (String)problems[problemNumber].get("question");
+						answer = (String)problems[problemNumber].get("answer");
+						hint1 = (String)problems[problemNumber].get("hint1");
+						hint2 = (String)problems[problemNumber].get("hint2");
+						problemNumber++;
+						sendToClient("Question | "+question);
+						incorrect = 0;
+					}
+					else{
+						sendToClient("[server] "+nickName+" Wrong answer!");
+						incorrect++;
+			
+						if(incorrect == 2){
+							sendToClient("Hint | "+hint1);
+						}
+						else if(incorrect == 4){
+							sendToClient("Hint | "+hint2);
+						}
+			
+						else if(incorrect == 10){
+							sendToClient("[server] No one can guess");
+							//give new question	to users
+							question = (String)problems[problemNumber].get("question");
+							answer = (String)problems[problemNumber].get("answer");
+							hint1 = (String)problems[problemNumber].get("hint1");
+							hint2 = (String)problems[problemNumber].get("hint2");
+							problemNumber++;
+							sendToClient("Question | "+question);
+							incorrect = 0;
+						}
+					}
+				}
+	
+			}catch(Exception e){e.printStackTrace();}
+
+		}
+
+
+	}	
+
+
+	class Quiz extends Thread{
+		String nickName;
+		Socket socket;
+		int score;
+	
+		public Quiz(String nickName, Socket socket,int score){
+			try{
+				this.nickName = nickName;
+				this.socket = socket;
+				this.score = score;
+			}catch(Exception e){e.printStackTrace();}
+		}
+		
+		public void run(){
+			try{
+				sendToClient("[Test]"+nickName+" score : "+score);
+			}catch(Exception e){e.printStackTrace();}
+		}
+	}
+
+
+	public synchronized void sendToClient(String msg){
+		try{
+			for(UserClass user : user_list){
+				user.dos.writeUTF(msg);
+			}
+
+		}catch(Exception e){e.printStackTrace();}
+
+	}
+
+
+}
